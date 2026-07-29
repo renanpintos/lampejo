9478e997babdf33113573fec88cb2bb7164b270c
renanpintos <renan.pinto.s@gmail.com>
2026-07-29 18:06:50 -0300

    Create menu list and update home layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,48 @@ function App() {
     alert("Lampejo funcionando no React!");
   };
 
-  const iniciarLampejo = () => {
-    window.location.href = `${import.meta.env.BASE_URL}Lampejo_index.html`;
+  const iniciarLampejo = (version) => {
+    window.location.href = `${import.meta.env.BASE_URL}Lampejo_index.html?version=${version}`;
   };
 
   return (
     <>
-    <button 
-        id="play-button" 
-        onClick={iniciarLampejo} 
-        style={{ cursor: 'pointer', border: 'none', background: 'transparent' }}
-      >
-        <img 
-          src={`${import.meta.env.BASE_URL}images/play_branco.jpg`}
-          alt="Botão Play" 
-          width="150" 
-          height="150" 
-        />
-      </button>
+    <div className="header">
+      <h1>Lampejo Web</h1>
+    </div>
+
+    <div className="card">
+    <button id="play-button1" className="play-button" onClick={() => iniciarLampejo('1')}>
+      <img src={`${import.meta.env.BASE_URL}images/play_branco.jpg`} alt="Play Lampejo 01"/>
+      Play Lampejo 01
+    </button>
+
+    <button id="play-button2" className="play-button" onClick={() => iniciarLampejo('2')}>
+      <img src={`${import.meta.env.BASE_URL}images/play_branco.jpg`} alt="Play Lampejo 02"/>
+      Play Lampejo 02
+    </button>
+
+    <button id="play-button3" className="play-button" onClick={() => iniciarLampejo('3')}>
+      <img src={`${import.meta.env.BASE_URL}images/play_branco.jpg`} alt="Play Lampejo 03"/>
+      Play Lampejo 03
+    </button>
+
+    <button id="play-button4" className="play-button" onClick={() => iniciarLampejo('4')}>
+      <img src={`${import.meta.env.BASE_URL}images/play_branco.jpg`} alt="Play Lampejo 04"/>
+      Play Lampejo 04
+    </button>
+
+    <button id="play-button5" className="play-button" onClick={() => iniciarLampejo('5')}>
+      <img src={`${import.meta.env.BASE_URL}images/play_branco.jpg`} alt="Play Lampejo 05"/>
+      Play Lampejo 05
+    </button>
+
+    <button id="play-button6" className="play-button" onClick={() => iniciarLampejo('6')}>
+      <img src={`${import.meta.env.BASE_URL}images/play_branco.jpg`} alt="Play Lampejo 06"/>
+      Play Lampejo 06
+    </button>
+
+    </div>
     </>
   )
 }
